ae77aac196176983798d13862a8a317a2c163ff0
rvitter <ryanvitter@0ptimus.com>
2018-02-10 15:44:00 -0500

    clean up container code

--- src/components/Layout/Container.vue
+++ src/components/Layout/Container.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="nav">
-    <!-- <v-toolbar color="green" dark fixed app>
-      <v-toolbar-title>Spotify Stats</v-toolbar-title>
-    </v-toolbar> -->
     <v-content>
       <v-container fluid fill-height>
         <v-layout
