54a63770128a435cd0db11db9f14c13543531169
rvitter <ryanvitter@0ptimus.com>
2018-02-10 15:34:24 -0500

    ad func for saving songs to library

--- src/app/Home.vue
+++ src/app/Home.vue
@@ -127,13 +127,13 @@
           })
         })
     },
-    audioUpdate () {
-      const audio = this.$refs.audio
-      if (audio.currentTime > 0 && !audio.paused && !audio.ended) {
-        this.audioPlaying = true
-      } else {
-        this.audioPlaying = false
-      }
+    saveSong (id) {
+      console.log(id)
+      const ids = {ids:[id]}
+      axios.put('https://api.spotify.com/v1/me/tracks/?ids=', ids, {
+        headers: {'Authorization': 'Bearer ' + this.accessToken}
+      }).then((res) => console.log(res))
+
     }
   }
 
